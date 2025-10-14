4a6ae92fffe3a0d5a0508e29e48ef56cd8f4acce
Ivars Umbrasko <ivarsakadarius@gmail.com>
2025-10-14 12:28:12 +0300

    Implemented init() function where stores triangle vertices and bind them to a buffer for rendering

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,63 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-int init(void) {
+using namespace std;
+
+#include "vgl.h"
+#include "LoadShaders.h"
+
+enum VAO_IDs
+{
+  Triangles,
+  NumVAOs
+};
+
+enum Buffer_IDs
+{
+  ArrayBuffer,
+  NumBuffers
+};
+
+enum Attrib_IDs
+{
+  vPosition = 0
+};
+
+GLuint VAOs[NumVAOs];
+GLuint Buffers[NumBuffers];
+const GLuint NumVertices = 6;
+
+int init(void)
+{
+  static const GLfloat vertices[NumVertices][2]{
+      {-0.90, -0.90},
+      {0.85, -0.90},
+      {-0.90, 0.85},
+      {0.90, -0.85},
+      {0.90, 0.90},
+      {-0.85, 0.90}};
+
+  glCreateVertexArrays(NumVAOs, VAOs);
+
+  glCreateBuffers(NumBuffers, Buffers);
+  glNamedBufferStorage(Buffers[NumBuffers], sizeof(vertices), vertices, 0);
+
+  ShaderInfo shaders[] = {
+      {GL_VERTEX_SHADER, "triangles.vert"},
+      {GL_FRAGMENT_SHADER, "triangles.frag"},
+      {GL_NONE, NULL}};
+
+  GLuint program = LoadShaders(shaders);
+  glUseProgram(program);
+  glGenVertexArrays(NumVAOs, VAOs);
+  glBindVertexArray(VAOs[Triangles]);
+  glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
+  glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+  glEnableVertexAttribArray(vPosition);
+
+
 
 }
-
-
 
 int main(int argc, char **argv)
 {
@@ -18,7 +70,6 @@ int main(int argc, char **argv)
   {
     glfwSwapBuffers(window);
     glfwPollEvents();
-
   }
 
   glfwDestroyWindow(window);
